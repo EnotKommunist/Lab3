cbd746eb3dd5341d2f27c7f15769185a98fdda22
Alex <94539881+EnotKommunist@users.noreply.github.com>
2026-03-10 10:15:32 +0500

    Update 3_2.fs

--- laba3_2/3_2.fs
+++ laba3_2/3_2.fs
@@ -1,17 +1,40 @@
 open System
 
+/// Функция для безопасного ввода целого числа
+let rec readInt() =
+    match Int32.TryParse(Console.ReadLine()) with
+    | true, value -> value
+    | false, _ -> 
+        printfn "Ошибка! Введите целое число."
+        readInt()
+
+/// Функция для ввода количества элементов
+let rec inputCount() =
+    printf "Введите кол-во эл списка: "
+    let count = readInt()
+    if count <= 0 then
+        printfn "Количество элементов должно быть положительным!"
+        inputCount()
+    else
+        count
+
+/// Функция для ввода списка чисел
+let inputNumbers count =
+    printfn "Введите эл списка: "
+    [ for _ in 1..count -> readInt() ]
+
+/// Функция для ввода искомого числа
+let inputRequiredNumber() =
+    printf "Какое число необходимо найти: "
+    readInt()
+
 /// проверка на наличие необходимого числа
 let rec recNum num requiredNumber = 
     if num < 10 then
-        if num = requiredNumber then
-            1
-        else
-            0
+        if num = requiredNumber then 1 else 0
     else
-        if num % 10 = requiredNumber then
-            1
-        else
-            recNum (num/10) requiredNumber
+        if num % 10 = requiredNumber then 1
+        else recNum (num/10) requiredNumber
 
 /// Перебор элементов в seq
 let findNum numbers requiredNumber = 
@@ -20,26 +43,17 @@ let findNum numbers requiredNumber =
             yield recNum (abs(num)) requiredNumber
     }
 
+/// Функция для подсчета количества чисел с искомой цифрой
+let calculateCount numbers requiredNumber =
+    let numbersSeq = numbers |> Seq.ofList
+    let listNum = findNum numbersSeq requiredNumber
+    Seq.fold (fun acc x -> acc + x) 0 listNum    
+
 [<EntryPoint>]
 let main args = 
-    printf "Введите кол-во эл списка: "
-    let countEl = int(Console.ReadLine())
-    if countEl > 0 then
-        printfn "Введите эл списка: "
-        let numbers = [
-            for i in 0..countEl-1 -> 
-                int(Console.ReadLine())
-        ]
-        printf "Какое число необходимо найти: "
-        let requiredNumber = int(Console.ReadLine())
-        let numbersSeq = numbers |> Seq.ofList
-        let listNum = findNum numbersSeq requiredNumber
-        let sum = 
-            Seq.fold 
-                (fun acc x -> acc + x) 
-                0 
-                listNum
-        printfn "Кол-во чисел с %i: %i" requiredNumber sum
-    else
-        printfn "Некорректный ввод"
+    let countEl = inputCount()
+    let numbers = inputNumbers countEl
+    let requiredNumber = inputRequiredNumber()
+    let sum = calculateCount numbers requiredNumber
+    printfn "Кол-во чисел с %i: %i" requiredNumber sum
     0
