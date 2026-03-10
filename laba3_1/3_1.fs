3378cb65bb99190d70360948dd3ad8a19bd4edb0
Alex <94539881+EnotKommunist@users.noreply.github.com>
2026-03-10 10:14:23 +0500

    Update 3_1.fs

--- laba3_1/3_1.fs
+++ laba3_1/3_1.fs
@@ -13,19 +13,39 @@ let rec minNum num min =
 let findMinNum numbers =
     numbers |> Seq.map (fun x -> minNum x 10)
 
+/// Функция для безопасного ввода целого числа
+let rec readInt() =
+    match Int32.TryParse(Console.ReadLine()) with
+    | true, value -> value
+    | false, _ -> 
+        printfn "Ошибка! Введите целое число."
+        readInt()
+
+/// Функция для ввода количества элементов
+let rec inputCount() =
+    printf "Введите кол-во эл списка: "
+    let count = readInt()
+    if count <= 0 then
+        printfn "Количество элементов должно быть положительным!"
+        inputCount()  // Рекурсивный запрос при ошибке
+    else
+        count
+
+/// Функция для ввода списка чисел
+let inputNumbers count =
+    printfn "Введите эл списка: "
+    [ for _ in 1 .. count -> readInt() ]
+
+/// Функция для вычисления и вывода результатов
+let processNumbers numbers =
+    let numbersSeq = numbers |> Seq.ofList
+    let minDigits = findMinNum numbersSeq
+    printfn "Исходные числа: %A" numbers
+    printfn "Минимальные цифры: %A" (minDigits |> Seq.toList)
+
 [<EntryPoint>]
 let main argv =
-    printf "Введите кол-во эл списка: "
-    let countEl = int (Console.ReadLine())
-    
-    if countEl > 0 then
-        printfn "Введите эл списка: "
-        let numbersList = [ for i in 0 .. countEl - 1 -> int(Console.ReadLine()) ]
-        let numbersSeq = numbersList |> Seq.ofList
-        let minDigits = findMinNum numbersSeq
-        printfn "Исходные числа: %A" numbersList
-        printfn "Минимальные цифры: %A" (minDigits |> Seq.toList)
-    else
-        printfn "Некорректный ввод"
-    
+    let countEl = inputCount()
+    let numbersList = inputNumbers countEl
+    processNumbers numbersList
     0
